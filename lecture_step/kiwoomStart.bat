a86efc4bca17f8a37e444986794fb8c63e327a2d
SUNGBEOM AHN <sungbeom78@naver.com>
2020-11-02 18:24:59 +0900

    job upload

---
+++ lecture_step/kiwoomStart.bat
@@ -0,0 +1,9 @@
+@ECHO ON
+title Kiwoom Start
+
+cd C:\Users\sungb\PycharmProjects\lecture_step
+
+call activate py37_32
+python __init__.py
+
+cmd.exe
---
+++ lecture_step/ui/ui.py
@@ -0,0 +1,13 @@
+from kiwoom.kiwoom import *
+import sys
+from PyQt5.QtWidgets import *
+
+class UI_class():
+	def __init__(self):
+		print("UI_class 입니다.")
+
+		self.app = QApplication(sys.argv)
+
+		self.ki = Kiwoom()
+
+		self.app.exec_()
